51f9f6a3766a600fc79d0857b0d493fa82a91256
CryptorGit <cryptor@outlook.jp>
2025-06-22 16:44:00 +0900

    errorを修正

--- MiniMikuDanceMaui/MauiProgram.cs
+++ MiniMikuDanceMaui/MauiProgram.cs
@@ -1,5 +1,6 @@
 using Microsoft.Maui;
 using Microsoft.Maui.Hosting;
+using Microsoft.Maui.Controls.Hosting;
 
 namespace MiniMikuDanceMaui;
 
@@ -8,8 +9,14 @@ public static class MauiProgram
     public static MauiApp CreateMauiApp()
     {
         var builder = MauiApp.CreateBuilder();
+
         builder
-            .UseMauiApp<App>();
+            // ← 型パラメータで自分の App クラスを渡す
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            });
 
         return builder.Build();
     }
