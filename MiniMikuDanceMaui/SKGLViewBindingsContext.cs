479d17ed64b885790701b1481d6f84eb99d1036f
CryptorGit <167396277+CryptorGit@users.noreply.github.com>
2025-06-23 21:14:19 +0900

    feat: add SKGLView bindings context

---
+++ MiniMikuDanceMaui/SKGLViewBindingsContext.cs
@@ -0,0 +1,13 @@
+using System;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace MiniMikuDanceMaui;
+
+internal class SKGLViewBindingsContext : IBindingsContext
+{
+    public nint GetProcAddress(string procName)
+    {
+        return GLFW.GetProcAddress(procName);
+    }
+}
