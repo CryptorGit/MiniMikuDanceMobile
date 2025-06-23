ef3c06d00e216eb23d00473c62fbdf3924fc23e7
CryptorGit <cryptor@outlook.jp>
2025-06-23 21:19:38 +0900

    ビルドによるコミット

--- MiniMikuDanceMaui/obj/Debug/net8.0-android34.0/MiniMikuDanceMaui.AssemblyInfo.cs
+++ MiniMikuDanceMaui/obj/Debug/net8.0-android34.0/MiniMikuDanceMaui.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiniMikuDanceMaui")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d36edafc7a8be8fcc9a402a7df269c51a758b72")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ee2045ce8c78a72b4bd18fb1c95cb4bd44e3a3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiniMikuDanceMaui")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiniMikuDanceMaui")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
