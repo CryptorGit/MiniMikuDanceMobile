9c74958162dad24bcd4044d3489482a1c0985208
CryptorGit <cryptor@outlook.jp>
2025-06-29 20:35:19 +0900

    Empty main branch: remove all files

--- AppCore/Util/Singleton.cs
+++
@@ -1,8 +0,0 @@
-namespace MiniMikuDance.Util;
-
-public abstract class Singleton<T> where T : class, new()
-{
-    private static readonly Lazy<T> _instance = new(() => new T());
-
-    public static T Instance => _instance.Value;
-}
