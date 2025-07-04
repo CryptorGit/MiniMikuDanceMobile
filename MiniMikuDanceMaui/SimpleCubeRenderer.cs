6c6b641ea2e59960f727a5b81bada20b367d625d
CryptorGit <167396277+CryptorGit@users.noreply.github.com>
2025-07-05 02:10:02 +0900

    fix texture loading strongly typed

--- MiniMikuDanceMaui/SimpleCubeRenderer.cs
+++ MiniMikuDanceMaui/SimpleCubeRenderer.cs
@@ -279,10 +279,10 @@ void main(){
             var handle = GCHandle.Alloc(data.TextureData, GCHandleType.Pinned);
             try
             {
-                GL.TexImage2D((All)TextureTarget.Texture2D, 0,
-                    (All)PixelInternalFormat.Rgba,
+                GL.TexImage2D(TextureTarget.Texture2D, 0,
+                    PixelInternalFormat.Rgba,
                     data.TextureWidth, data.TextureHeight, 0,
-                    (All)PixelFormat.Rgba, (All)PixelType.UnsignedByte,
+                    PixelFormat.Rgba, PixelType.UnsignedByte,
                     handle.AddrOfPinnedObject());
             }
             finally
@@ -297,10 +297,10 @@ void main(){
             var handle = GCHandle.Alloc(white, GCHandleType.Pinned);
             try
             {
-                GL.TexImage2D((All)TextureTarget.Texture2D, 0,
-                    (All)PixelInternalFormat.Rgba,
+                GL.TexImage2D(TextureTarget.Texture2D, 0,
+                    PixelInternalFormat.Rgba,
                     1, 1, 0,
-                    (All)PixelFormat.Rgba, (All)PixelType.UnsignedByte,
+                    PixelFormat.Rgba, PixelType.UnsignedByte,
                     handle.AddrOfPinnedObject());
             }
             finally
