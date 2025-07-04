bd11db7cf720cd1b9c1ac007aadfd822cf68ca61
CryptorGit <167396277+CryptorGit@users.noreply.github.com>
2025-07-05 01:00:52 +0900

    Improve VRM picker and explorer

--- MiniMikuDanceMaui/CameraPage.xaml.cs
+++ MiniMikuDanceMaui/CameraPage.xaml.cs
@@ -276,8 +276,9 @@ public partial class CameraPage : ContentPage
     {
         base.OnAppearing();
 #if ANDROID
-        var status = await Permissions.RequestAsync<Permissions.StorageWrite>();
-        if (status != PermissionStatus.Granted)
+        var readStatus = await Permissions.RequestAsync<Permissions.StorageRead>();
+        var writeStatus = await Permissions.RequestAsync<Permissions.StorageWrite>();
+        if (readStatus != PermissionStatus.Granted || writeStatus != PermissionStatus.Granted)
         {
             LogService.WriteLine("[CameraPage] Storage permission denied");
         }
@@ -458,7 +459,13 @@ public partial class CameraPage : ContentPage
         {
             var result = await FilePicker.Default.PickAsync(new PickOptions
             {
-                PickerTitle = "Select VRM file"
+                PickerTitle = "Select VRM file",
+                FileTypes = new FilePickerFileType(new Dictionary<DevicePlatform, IEnumerable<string>>
+                {
+                    [DevicePlatform.Android] = new[] { "application/octet-stream", ".vrm" },
+                    [DevicePlatform.WinUI] = new[] { ".vrm" },
+                    [DevicePlatform.iOS] = new[] { ".vrm" }
+                })
             });
 
             if (result != null)
@@ -471,8 +478,9 @@ public partial class CameraPage : ContentPage
 
                 LogService.WriteLine($"Model selected: {result.FileName}");
 
+                await using var stream = await result.OpenReadAsync();
                 var importer = new MiniMikuDance.Import.ModelImporter();
-                var data = importer.ImportModel(result.FullPath);
+                var data = importer.ImportModel(stream);
                 _renderer.LoadModel(data);
                 Viewer?.InvalidateSurface();
             }
@@ -498,7 +506,7 @@ public partial class CameraPage : ContentPage
             else if (name == "Open")
             {
                 var modelsPath = MmdFileSystem.Ensure("Models");
-                var ev = new ExplorerView(modelsPath);
+                var ev = new ExplorerView(modelsPath, new[] { ".vrm" });
                 ev.FileSelected += OnOpenExplorerFileSelected;
                 ev.LoadDirectory(modelsPath);
                 view = ev;
@@ -636,7 +644,9 @@ public partial class CameraPage : ContentPage
                 PickerTitle = "Select VRM file",
                 FileTypes = new FilePickerFileType(new Dictionary<DevicePlatform, IEnumerable<string>>
                 {
-                    [DevicePlatform.Android] = new[] { ".vrm" }
+                    [DevicePlatform.Android] = new[] { "application/octet-stream", ".vrm" },
+                    [DevicePlatform.WinUI] = new[] { ".vrm" },
+                    [DevicePlatform.iOS] = new[] { ".vrm" }
                 })
             });
 
