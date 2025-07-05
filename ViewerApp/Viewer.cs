b428aefc6b86562bcfe13c0906533cbe16bd4c3a
CryptorGit <167396277+CryptorGit@users.noreply.github.com>
2025-07-05 20:34:54 +0900

    fix texture upload enumerations

--- ViewerApp/Viewer.cs
+++ ViewerApp/Viewer.cs
@@ -159,7 +159,8 @@ public class Viewer : IDisposable
                 }
             }
             GL.PixelStore(PixelStoreParameter.UnpackAlignment, 4);
-            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
+            GL.GenerateMipmap(GenerateMipmapTarget.Texture2D);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.LinearMipmapLinear);
             GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
             GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)TextureWrapMode.Repeat);
             GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)TextureWrapMode.Repeat);
