25b98086b5276b21e331acd46abd04e35bd445bc
CryptorGit <167396277+CryptorGit@users.noreply.github.com>
2025-07-06 23:11:16 +0900

    Add BlazePoseJoint enum and update MotionApplier

--- AppCore/Motion/MotionApplier.cs
+++ AppCore/Motion/MotionApplier.cs
@@ -1,4 +1,5 @@
 using System.Numerics;
+using System.Collections.Generic;
 using MiniMikuDance.Import;
 using MiniMikuDance.PoseEstimation;
 
@@ -7,21 +8,60 @@ namespace MiniMikuDance.Motion;
 public class MotionApplier
 {
     private readonly ModelData _model;
+    private readonly Dictionary<BlazePoseJoint, int> _boneMap = new();
 
     public MotionApplier(ModelData model)
     {
         _model = model;
+        MapBones();
+    }
+
+    private void MapBones()
+    {
+        void AddMap(BlazePoseJoint joint, string boneName)
+        {
+            if (_model.HumanoidBones.TryGetValue(boneName, out int idx))
+            {
+                _boneMap[joint] = idx;
+            }
+        }
+
+        AddMap(BlazePoseJoint.LeftShoulder, "leftShoulder");
+        AddMap(BlazePoseJoint.RightShoulder, "rightShoulder");
+        AddMap(BlazePoseJoint.LeftElbow, "leftLowerArm");
+        AddMap(BlazePoseJoint.RightElbow, "rightLowerArm");
+        AddMap(BlazePoseJoint.LeftWrist, "leftHand");
+        AddMap(BlazePoseJoint.RightWrist, "rightHand");
+        AddMap(BlazePoseJoint.LeftHip, "leftUpperLeg");
+        AddMap(BlazePoseJoint.RightHip, "rightUpperLeg");
+        AddMap(BlazePoseJoint.LeftKnee, "leftLowerLeg");
+        AddMap(BlazePoseJoint.RightKnee, "rightLowerLeg");
+        AddMap(BlazePoseJoint.LeftAnkle, "leftFoot");
+        AddMap(BlazePoseJoint.RightAnkle, "rightFoot");
+        AddMap(BlazePoseJoint.Nose, "head");
     }
 
     public void Apply(JointData joint)
     {
-        if (joint.Positions.Length == 0)
-            return;
+        foreach (var kv in _boneMap)
+        {
+            int jIndex = (int)kv.Key;
+            if (jIndex >= joint.Positions.Length)
+                continue;
+            int bIndex = kv.Value;
+            if (bIndex >= 0 && bIndex < _model.Bones.Count)
+            {
+                _model.Bones[bIndex].Translation = joint.Positions[jIndex];
+            }
+        }
 
-        var pos = joint.Positions[0];
-        var rotAngle = joint.Positions.Length > 1 ? joint.Positions[1].Y : 0f;
-        var trans = Matrix4x4.CreateTranslation(pos);
-        var rot = Matrix4x4.CreateFromAxisAngle(Vector3.UnitY, rotAngle);
-        _model.Transform = rot * trans;
+        if (joint.Positions.Length > (int)BlazePoseJoint.RightHip)
+        {
+            var lh = joint.Positions[(int)BlazePoseJoint.LeftHip];
+            var rh = joint.Positions[(int)BlazePoseJoint.RightHip];
+            var hip = (lh + rh) * 0.5f;
+            var trans = Matrix4x4.CreateTranslation(hip);
+            _model.Transform = trans;
+        }
     }
 }
