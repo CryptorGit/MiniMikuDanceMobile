e9e5a53c129818196eeb17c41af7b2ac22369a9a
CryptorGit <167396277+CryptorGit@users.noreply.github.com>
2025-07-09 00:01:01 +0900

    Update timeline layout and scrolling

--- MiniMikuDanceMaui/TimeLineView.xaml.cs
+++ MiniMikuDanceMaui/TimeLineView.xaml.cs
@@ -1,6 +1,7 @@
 using Microsoft.Maui.Controls;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using MiniMikuDance.Motion;
 
 namespace MiniMikuDanceMaui;
@@ -12,6 +13,8 @@ public partial class TimeLineView : ContentView
     public event Action? EditKeyClicked;
     public event Action? DeleteKeyClicked;
 
+    private bool _syncingScroll;
+
     public TimeLineView()
     {
         InitializeComponent();
@@ -20,6 +23,8 @@ public partial class TimeLineView : ContentView
         EditKeyButton.Clicked += (s, e) => EditKeyClicked?.Invoke();
         DeleteKeyButton.Clicked += (s, e) => DeleteKeyClicked?.Invoke();
         FrameScaleEntry.Text = "10";
+        GridScroll.Scrolled += OnGridScrolled;
+        BoneList.Scrolled += OnBoneListScrolled;
     }
 
     public int FrameScale
@@ -29,11 +34,32 @@ public partial class TimeLineView : ContentView
     }
 
     public void SetBones(IEnumerable<string> bones)
-        => GridView.SetBones(bones);
+    {
+        var list = bones.ToList();
+        GridView.SetBones(list);
+        BoneList.ItemsSource = list;
+    }
 
     public void SetMotion(MotionEditor? editor, MotionPlayer? player)
     {
         GridView.MotionEditor = editor;
         GridView.MotionPlayer = player;
+    }
+
+    private async void OnGridScrolled(object? sender, ScrolledEventArgs e)
+    {
+        if (_syncingScroll) return;
+        _syncingScroll = true;
+        int index = (int)(e.ScrollY / GridView.RowHeight);
+        await BoneList.ScrollToAsync(index, -1, ScrollToPosition.Start, false);
+        _syncingScroll = false;
+    }
+
+    private async void OnBoneListScrolled(object? sender, ItemsViewScrolledEventArgs e)
+    {
+        if (_syncingScroll) return;
+        _syncingScroll = true;
+        await GridScroll.ScrollToAsync(GridScroll.ScrollX, e.VerticalOffset, false);
+        _syncingScroll = false;
     }
 }
