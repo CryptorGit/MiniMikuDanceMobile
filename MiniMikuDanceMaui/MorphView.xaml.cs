b238b02a6bd315954f3d3d643d1c43a2509c43fe
CryptorGit <167396277+CryptorGit@users.noreply.github.com>
2025-08-07 20:55:25 +0900

    MorphViewのグリッドの追加方法を修正

--- MiniMikuDanceMaui/MorphView.xaml.cs
+++ MiniMikuDanceMaui/MorphView.xaml.cs
@@ -21,10 +21,23 @@ public partial class MorphView : ContentView
         foreach (var morph in morphs)
         {
             var name = morph.Name;
-            var grid = new Grid { ColumnDefinitions = "*,60", RowSpacing = 2 };
-            grid.Children.Add(new Label { Text = name, TextColor = textColor }, 0, 0);
+            var grid = new Grid
+            {
+                ColumnDefinitions = new ColumnDefinitionCollection
+                {
+                    new ColumnDefinition { Width = GridLength.Star },
+                    new ColumnDefinition { Width = 60 }
+                },
+                RowSpacing = 2
+            };
+            var nameLabel = new Label { Text = name, TextColor = textColor };
+            grid.Add(nameLabel);
+            Grid.SetColumn(nameLabel, 0);
+            Grid.SetRow(nameLabel, 0);
             var valueLabel = new Label { Text = "0", TextColor = textColor, HorizontalTextAlignment = TextAlignment.End };
-            grid.Children.Add(valueLabel, 1, 0);
+            grid.Add(valueLabel);
+            Grid.SetColumn(valueLabel, 1);
+            Grid.SetRow(valueLabel, 0);
             MorphList.Children.Add(grid);
             var slider = new Slider { Minimum = 0, Maximum = 1 };
             slider.ValueChanged += (s, e) =>
