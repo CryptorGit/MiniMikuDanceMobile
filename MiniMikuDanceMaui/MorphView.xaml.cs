837b2c98fa981d4d554ddd6074dc401b5897b6c9
CryptorGit <167396277+CryptorGit@users.noreply.github.com>
2025-08-08 23:58:35 +0900

    Refactor MorphView to use CollectionView

--- MiniMikuDanceMaui/MorphView.xaml.cs
+++ MiniMikuDanceMaui/MorphView.xaml.cs
@@ -1,73 +1,94 @@
 using Microsoft.Maui.Controls;
 using Microsoft.Maui.Dispatching;
+using MiniMikuDance.Import;
 using System;
 using System.Collections.Generic;
-using MiniMikuDance.Import;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 
 namespace MiniMikuDanceMaui;
 
 public partial class MorphView : ContentView
 {
+    public ObservableCollection<MorphItem> Morphs { get; } = new();
+
     public event Action<string, double>? MorphValueChanged;
+
     private readonly Dictionary<string, (IDispatcherTimer timer, double last)> _debouncers = new();
 
     public MorphView()
     {
         InitializeComponent();
+        BindingContext = this;
     }
 
     public void SetMorphs(IEnumerable<MorphData> morphs)
     {
-        MorphList.Children.Clear();
-        var textColor = (Color)(Application.Current?.Resources?.TryGetValue("TextColor", out var color) == true ? color : Colors.Black);
+        foreach (var m in Morphs)
+        {
+            m.PropertyChanged -= MorphItem_PropertyChanged;
+        }
+        Morphs.Clear();
         foreach (var morph in morphs)
         {
-            var name = morph.Name;
-            var grid = new Grid
-            {
-                ColumnDefinitions = new ColumnDefinitionCollection
-                {
-                    new ColumnDefinition { Width = GridLength.Star },
-                    new ColumnDefinition { Width = 60 }
-                },
-                RowSpacing = 2
-            };
-            var nameLabel = new Label { Text = name, TextColor = textColor };
-            grid.Add(nameLabel);
-            Grid.SetColumn(nameLabel, 0);
-            Grid.SetRow(nameLabel, 0);
-            var valueLabel = new Label { Text = "0", TextColor = textColor, HorizontalTextAlignment = TextAlignment.End };
-            grid.Add(valueLabel);
-            Grid.SetColumn(valueLabel, 1);
-            Grid.SetRow(valueLabel, 0);
-            MorphList.Children.Add(grid);
-            var slider = new Slider { Minimum = 0, Maximum = 1 };
-            slider.ValueChanged += (s, e) =>
-            {
-                valueLabel.Text = $"{e.NewValue:F2}";
-                // Debounce updates to reduce CPU churn while dragging
-                if (!_debouncers.TryGetValue(name, out var entry))
-                {
-                    var t = Dispatcher.CreateTimer();
-                    t.Interval = TimeSpan.FromMilliseconds(16);
-                    t.IsRepeating = false;
-                    t.Tick += (ss, ee) =>
-                    {
-                        MorphValueChanged?.Invoke(name, _debouncers[name].last);
-                    };
-                    entry = (t, e.NewValue);
-                    _debouncers[name] = entry;
-                }
-                else
-                {
-                    entry.last = e.NewValue;
-                    _debouncers[name] = entry;
-                }
-                // restart single-shot timer
-                entry.timer.Stop();
-                entry.timer.Start();
-            };
-            MorphList.Children.Add(slider);
+            var item = new MorphItem(morph.Name);
+            item.PropertyChanged += MorphItem_PropertyChanged;
+            Morphs.Add(item);
         }
     }
+
+    private void MorphItem_PropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        if (sender is not MorphItem item || e.PropertyName != nameof(MorphItem.Value))
+        {
+            return;
+        }
+
+        var name = item.Name;
+        var value = item.Value;
+        if (!_debouncers.TryGetValue(name, out var entry))
+        {
+            var t = Dispatcher.CreateTimer();
+            t.Interval = TimeSpan.FromMilliseconds(16);
+            t.IsRepeating = false;
+            t.Tick += (s, _) => MorphValueChanged?.Invoke(name, _debouncers[name].last);
+            entry = (t, value);
+            _debouncers[name] = entry;
+        }
+        else
+        {
+            entry.last = value;
+            _debouncers[name] = entry;
+        }
+        entry.timer.Stop();
+        entry.timer.Start();
+    }
 }
+
+public class MorphItem : INotifyPropertyChanged
+{
+    public string Name { get; }
+
+    private double _value;
+    public double Value
+    {
+        get => _value;
+        set
+        {
+            if (_value == value)
+            {
+                return;
+            }
+            _value = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Value)));
+        }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    public MorphItem(string name)
+    {
+        Name = name;
+    }
+}
+
