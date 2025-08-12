0e2c8896be69b82775e395ae1d9e05c072640198
CryptorGit <cryptor@outlook.jp>
2025-08-13 03:43:29 +0900

    mmdツールを追加

---
+++ Documents/libmmd-for-unity-master/Assets/LibMmdDemo/BasicDemoController.cs
@@ -0,0 +1,43 @@
+﻿using LibMMD.Unity3D;
+using UnityEngine;
+
+namespace LibMmdDemo
+{
+	public class BasicDemoController : MonoBehaviour
+	{
+		public string ModelPath;
+
+		public string MotionPath;
+		
+		public string CameraPath;
+		
+		protected void Start ()
+		{
+			if (string.IsNullOrEmpty(ModelPath) || string.IsNullOrEmpty(MotionPath) || string.IsNullOrEmpty(CameraPath))
+			{
+				Debug.LogError("please fill your model, motion and camera file path");
+			}
+			var mmdObj = MmdGameObject.CreateGameObject("MmdGameObject");
+			var mmdGameObject = mmdObj.GetComponent<MmdGameObject>();
+
+			mmdGameObject.LoadModel(ModelPath);
+			mmdGameObject.LoadMotion(MotionPath);
+			
+			//You can set model render options
+			mmdGameObject.UpdateConfig(new MmdUnityConfig
+			{
+				EnableDrawSelfShadow = MmdConfigSwitch.ForceFalse,
+				EnableCastShadow = MmdConfigSwitch.ForceFalse
+			});	
+			
+			mmdGameObject.Playing = true;
+
+			var mmdCamera = MmdCameraObject.CreateGameObject("MmdCameraObject").GetComponent<MmdCameraObject>();
+			mmdCamera.LoadCameraMotion(CameraPath);
+			mmdCamera.Playing = true;
+
+		}
+
+		
+	}
+}
