58ea032cdf6dd6b44c324e2a6203fd1a8ee4f7c1
CryptorGit <167396277+CryptorGit@users.noreply.github.com>
2025-08-13 22:46:45 +0900

    MaterialExtension を独自実装に置き換え

--- MiniMikuDanceMaui/Extensions/MaterialExtension.cs
+++ MiniMikuDanceMaui/Extensions/MaterialExtension.cs
@@ -1,8 +1,20 @@
+using System;
 using Microsoft.Maui.Controls.Xaml;
+using MauiIcons.Material;
 
 namespace MiniMikuDanceMaui.Extensions;
 
 [AcceptEmptyServiceProvider]
-public class MaterialExtension : MauiIcons.Material.MaterialExtension
+public class MaterialExtension : IMarkupExtension
 {
+    public MaterialIcons Icon { get; set; }
+
+    public object ProvideValue(IServiceProvider serviceProvider)
+    {
+        var extension = new MauiIcons.Material.MaterialExtension
+        {
+            Icon = Icon
+        };
+        return ((IMarkupExtension)extension).ProvideValue(serviceProvider);
+    }
 }
