84b3b265e47632a380c50ba61d11680fc7b6e61e
CryptorGit <167396277+CryptorGit@users.noreply.github.com>
2025-08-14 15:43:00 +0900

    Add stub IK solver and PInvoke hooks

---
+++ Native/nanoem/emapp/IKSolver.h
@@ -0,0 +1,19 @@
+/*
+   Copyright (c) 2015-2023 hkrn All rights reserved
+
+   This file is part of emapp component and it's licensed under Mozilla Public License. see LICENSE.md for more details.
+*/
+
+#pragma once
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void nanoem_emapp_solve_ik(int32_t bone_index, const float position[3]);
+
+#ifdef __cplusplus
+}
+#endif
+
