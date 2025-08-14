5beca8244b922d676459eb199bef5e2be4f1411b
CryptorGit <167396277+CryptorGit@users.noreply.github.com>
2025-08-14 20:10:48 +0900

    feat: expose morph offsets

--- AppCore/Morph/MorphController.cs
+++ AppCore/Morph/MorphController.cs
@@ -4,11 +4,21 @@ namespace MiniMikuDance.Morph;
 
 public static class MorphController
 {
-    public static void SetWeight(IntPtr model, int index, float weight)
+    public static void SetWeight(IntPtr model, ReadOnlySpan<(int index, float weight)> morphs)
     {
         if (model == IntPtr.Zero)
             return;
-        NanoemMorph.SetMorphWeight(model, index, weight);
+        foreach (var (index, weight) in morphs)
+        {
+            NanoemMorph.SetMorphWeight(model, index, weight);
+        }
         NanoemMorph.Update(model);
+    }
+
+    public static void SetWeight(IntPtr model, int index, float weight)
+    {
+        Span<(int, float)> values = stackalloc (int, float)[1];
+        values[0] = (index, weight);
+        SetWeight(model, values);
     }
 }
