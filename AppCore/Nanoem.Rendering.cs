252eff07f913431ec7145df27de685fcd14094e9
CryptorGit <167396277+CryptorGit@users.noreply.github.com>
2025-08-14 16:29:09 +0900

    Add Rendering.cc stub

--- AppCore/Nanoem.Rendering.cs
+++ AppCore/Nanoem.Rendering.cs
@@ -6,19 +6,19 @@ internal static partial class Nanoem
 {
     private const string NativeLibName = "nanoem";
 
-    [DllImport(NativeLibName, CallingConvention = CallingConvention.Cdecl)]
-    private static extern void nanoemRenderingInitialize(int width, int height);
+    [DllImport(NativeLibName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "nanoemRenderingInitialize")]
+    private static extern void RenderingInitializeNative(int width, int height);
 
-    [DllImport(NativeLibName, CallingConvention = CallingConvention.Cdecl)]
-    private static extern void nanoemRenderingUpdateFrame();
+    [DllImport(NativeLibName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "nanoemRenderingUpdateFrame")]
+    private static extern void RenderingUpdateFrameNative();
 
     public static void RenderingInitialize(int width, int height)
     {
-        nanoemRenderingInitialize(width, height);
+        RenderingInitializeNative(width, height);
     }
 
     public static void RenderingUpdateFrame()
     {
-        nanoemRenderingUpdateFrame();
+        RenderingUpdateFrameNative();
     }
 }
