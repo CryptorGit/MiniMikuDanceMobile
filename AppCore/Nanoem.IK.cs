1255226b48148c67c9c290dd63d107265b4d9f33
CryptorGit <167396277+CryptorGit@users.noreply.github.com>
2025-08-14 17:03:49 +0900

    feat: add basic multi-constraint IK support

--- AppCore/Nanoem.IK.cs
+++ AppCore/Nanoem.IK.cs
@@ -5,19 +5,19 @@ namespace MiniMikuDance;
 internal static partial class Nanoem
 {
     [DllImport(NativeLibName, CallingConvention = CallingConvention.Cdecl)]
-    private static extern void nanoem_emapp_initialize_ik();
+    private static extern void nanoem_emapp_initialize_ik(int constraintCount);
 
     // position: input target and output effector position
     [DllImport(NativeLibName, CallingConvention = CallingConvention.Cdecl)]
-    private static extern void nanoem_emapp_solve_ik(int boneIndex, float[] position);
+    private static extern void nanoem_emapp_solve_ik(int constraintIndex, int boneIndex, float[] position);
 
-    public static void InitializeIk()
+    public static void InitializeIk(int constraintCount)
     {
-        nanoem_emapp_initialize_ik();
+        nanoem_emapp_initialize_ik(constraintCount);
     }
 
-    public static void SolveIk(int boneIndex, float[] position)
+    public static void SolveIk(int constraintIndex, int boneIndex, float[] position)
     {
-        nanoem_emapp_solve_ik(boneIndex, position);
+        nanoem_emapp_solve_ik(constraintIndex, boneIndex, position);
     }
 }
