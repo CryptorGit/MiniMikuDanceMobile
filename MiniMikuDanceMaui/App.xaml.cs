c9e8b728bf021227244e0c5a036d1f731dd0207a
CryptorGit <167396277+CryptorGit@users.noreply.github.com>
2025-08-15 23:32:01 +0900

    fix: カレントディレクトリ設定時の例外処理を追加

--- MiniMikuDanceMaui/App.xaml.cs
+++ MiniMikuDanceMaui/App.xaml.cs
@@ -4,6 +4,8 @@ using MiniMikuDance.Data;
 using MiniMikuDance.UI;
 using System;
 using System.IO;
+using Microsoft.Maui.ApplicationModel;
+using Microsoft.Maui.Storage;
 using MiniMikuDanceMaui.Storage;
 using MiniMikuDanceMaui.Views;
 
@@ -17,7 +19,23 @@ public partial class App : Application, IDisposable
     {
         InitializeComponent();
 
-        Directory.SetCurrentDirectory(MmdFileSystem.BaseDir);
+        try
+        {
+            Directory.SetCurrentDirectory(MmdFileSystem.BaseDir);
+        }
+        catch (Exception)
+        {
+            Directory.SetCurrentDirectory(FileSystem.AppDataDirectory);
+            MainThread.BeginInvokeOnMainThread(async () =>
+            {
+                var page = Application.Current?.MainPage;
+                if (page is not null)
+                {
+                    await page.DisplayAlert("権限エラー", "ディレクトリへのアクセスに失敗しました。設定から権限を再設定してください。", "OK");
+                }
+            });
+        }
+
         var uiConfig = DataManager.Instance.LoadConfig<UIConfig>("UIConfig");
         var bonesConfig = DataManager.Instance.LoadConfig<BonesConfig>("BonesConfig");
         Initializer.BonesConfig = bonesConfig;
