53c2b2d3008dd8715fd5e05c221621e6249f17b4
CryptorGit <167396277+CryptorGit@users.noreply.github.com>
2025-08-15 11:48:09 +0900

    PMXのIKと物理設定を読み込み

--- AppCore/Import/RigidBodyData.cs
+++ AppCore/Import/RigidBodyData.cs
@@ -1,3 +1,5 @@
+using System.Numerics;
+
 namespace MiniMikuDance.Import;
 
 public enum RigidBodyShape
@@ -7,18 +9,53 @@ public enum RigidBodyShape
     Capsule
 }
 
+public enum RigidBodyPhysicsType
+{
+    Static,
+    Dynamic,
+    DynamicAndBonePosition
+}
+
+public enum JointType
+{
+    Spring6DOF,
+    NoSpring6DOF,
+    P2P,
+    ConeTwist,
+    Slider,
+    Hinge
+}
+
 public class RigidBodyData
 {
     public string Name { get; set; } = string.Empty;
     public int BoneIndex { get; set; } = -1;
-    public float Mass { get; set; }
+    public byte Group { get; set; }
+    public ushort GroupTarget { get; set; }
     public RigidBodyShape Shape { get; set; }
+    public Vector3 Size { get; set; } = Vector3.Zero;
+    public Vector3 Position { get; set; } = Vector3.Zero;
+    public Vector3 Rotation { get; set; } = Vector3.Zero;
+    public float Mass { get; set; }
+    public float TranslationAttenuation { get; set; }
+    public float RotationAttenuation { get; set; }
+    public float Recoil { get; set; }
+    public float Friction { get; set; }
+    public RigidBodyPhysicsType PhysicsType { get; set; }
 }
 
 public class JointData
 {
     public string Name { get; set; } = string.Empty;
+    public JointType Type { get; set; }
     public int RigidBodyA { get; set; } = -1;
     public int RigidBodyB { get; set; } = -1;
+    public Vector3 Position { get; set; } = Vector3.Zero;
+    public Vector3 Rotation { get; set; } = Vector3.Zero;
+    public Vector3 TranslationMinLimit { get; set; } = Vector3.Zero;
+    public Vector3 TranslationMaxLimit { get; set; } = Vector3.Zero;
+    public Vector3 RotationMinLimit { get; set; } = Vector3.Zero;
+    public Vector3 RotationMaxLimit { get; set; } = Vector3.Zero;
+    public Vector3 TranslationSpring { get; set; } = Vector3.Zero;
+    public Vector3 RotationSpring { get; set; } = Vector3.Zero;
 }
-
