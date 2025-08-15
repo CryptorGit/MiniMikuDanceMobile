1f182c9891dff3fd9837febdf3f9a52454f6a2fe
CryptorGit <167396277+CryptorGit@users.noreply.github.com>
2025-08-15 16:06:03 +0900

    feat: add crash logging helper

--- MiniMikuDanceMaui/Platforms/Android/MainApplication.cs
+++ MiniMikuDanceMaui/Platforms/Android/MainApplication.cs
@@ -2,6 +2,7 @@ using Android.App;
 using Android.Runtime;
 using Microsoft.Maui;
 using Microsoft.Maui.Hosting;
+using MiniMikuDanceMaui.Helpers;
 
 namespace MiniMikuDanceMaui;
 
@@ -11,6 +12,13 @@ public class MainApplication : MauiApplication
     public MainApplication(System.IntPtr handle, JniHandleOwnership ownership)
         : base(handle, ownership)
     {
+    }
+
+    public override void OnCreate()
+    {
+        base.OnCreate();
+        CrashLogger.RegisterGlobal();
+        CrashLogger.RegisterAndroid();
     }
 
     protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
