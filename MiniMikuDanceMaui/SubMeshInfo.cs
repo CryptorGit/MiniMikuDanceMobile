084de8a77de629d7030e67de6aac2ac4e54fc47e
CryptorGit <cryptor@outlook.jp>
2025-08-16 18:18:10 +0900

    nanoem-project開始

--- MiniMikuDanceMaui/SubMeshInfo.cs
+++
@@ -1,8 +0,0 @@
-namespace MiniMikuDanceMaui;
-
-public class SubMeshInfo
-{
-    public int Index { get; set; }
-    public string Texture { get; set; } = string.Empty;
-    public string Size { get; set; } = string.Empty;
-}
