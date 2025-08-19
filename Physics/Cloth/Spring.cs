ed6579d16a6d6fc1954be1d270d22329238012f1
CryptorGit <167396277+CryptorGit@users.noreply.github.com>
2025-08-20 07:44:37 +0900

    feat: add cloth node and spring skeleton

---
+++ Physics/Cloth/Spring.cs
@@ -0,0 +1,11 @@
+namespace MiniMikuDance.Physics.Cloth;
+
+public struct Spring
+{
+    public int NodeA;
+    public int NodeB;
+    public float RestLength;
+    public float Stiffness;
+    public float Damping;
+}
+
