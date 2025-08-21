91c495827d31ced065e1eb8d695e31a1493a787d
CryptorGit <167396277+CryptorGit@users.noreply.github.com>
2025-08-22 00:27:04 +0900

    Cast TexImage2D parameters to All enums

--- MiniMikuDanceMaui/PmxRenderer.cs
+++ MiniMikuDanceMaui/PmxRenderer.cs
@@ -1143,14 +1143,14 @@ void main(){
                 try
                 {
                     GL.TexImage2D(
-                        TextureTarget.Texture2D,
+                        (All)TextureTarget.Texture2D,
                         0,
-                        PixelInternalFormat.Rgba,
+                        (All)PixelInternalFormat.Rgba,
                         sm.TextureWidth,
                         sm.TextureHeight,
                         0,
-                        PixelFormat.Rgba,
-                        PixelType.UnsignedByte,
+                        (All)PixelFormat.Rgba,
+                        (All)PixelType.UnsignedByte,
                         handle.AddrOfPinnedObject());
                 }
                 finally
@@ -1172,14 +1172,14 @@ void main(){
                 try
                 {
                     GL.TexImage2D(
-                        TextureTarget.Texture2D,
+                        (All)TextureTarget.Texture2D,
                         0,
-                        PixelInternalFormat.Rgba,
+                        (All)PixelInternalFormat.Rgba,
                         sm.SphereTextureWidth,
                         sm.SphereTextureHeight,
                         0,
-                        PixelFormat.Rgba,
-                        PixelType.UnsignedByte,
+                        (All)PixelFormat.Rgba,
+                        (All)PixelType.UnsignedByte,
                         handle.AddrOfPinnedObject());
                 }
                 finally
@@ -1201,14 +1201,14 @@ void main(){
                 try
                 {
                     GL.TexImage2D(
-                        TextureTarget.Texture2D,
+                        (All)TextureTarget.Texture2D,
                         0,
-                        PixelInternalFormat.Rgba,
+                        (All)PixelInternalFormat.Rgba,
                         sm.ToonTextureWidth,
                         sm.ToonTextureHeight,
                         0,
-                        PixelFormat.Rgba,
-                        PixelType.UnsignedByte,
+                        (All)PixelFormat.Rgba,
+                        (All)PixelType.UnsignedByte,
                         handle.AddrOfPinnedObject());
                 }
                 finally
