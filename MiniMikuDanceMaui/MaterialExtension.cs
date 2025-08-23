fd5f115a86f0bc92556f301f79b5b279694b5530
CryptorGit <167396277+CryptorGit@users.noreply.github.com>
2025-08-23 11:18:35 +0900

    feat: add AcceptEmptyServiceProvider to MaterialExtension

---
+++ MiniMikuDanceMaui/MaterialExtension.cs
@@ -0,0 +1,11 @@
+using Microsoft.Maui.Controls.Xaml;
+using MauiIcons.Core;
+
+[assembly: XmlnsDefinition("http://www.aathifmahir.com/dotnet/2022/maui/icons", "MauiIcons.Material")]
+
+namespace MauiIcons.Material;
+
+[AcceptEmptyServiceProvider]
+public sealed class MaterialExtension : BaseIconExtension<MaterialIcons>
+{
+}
