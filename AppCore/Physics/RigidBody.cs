2470206e015d80bc1e04a665605e45d5407d5320
CryptorGit <167396277+CryptorGit@users.noreply.github.com>
2025-08-26 00:03:59 +0900

    Add joint local anchors for physics

--- AppCore/Physics/RigidBody.cs
+++ AppCore/Physics/RigidBody.cs
@@ -34,7 +34,6 @@ public class Joint
     public string Name { get; set; } = string.Empty;
     public int RigidBodyA { get; set; } = -1;
     public int RigidBodyB { get; set; } = -1;
-    public Vector3 Position { get; set; } = Vector3.Zero;
     public Vector3 Rotation { get; set; } = Vector3.Zero;
     public Vector3 PositionMin { get; set; } = Vector3.Zero;
     public Vector3 PositionMax { get; set; } = Vector3.Zero;
@@ -42,4 +41,6 @@ public class Joint
     public Vector3 RotationMax { get; set; } = Vector3.Zero;
     public Vector3 SpringPosition { get; set; } = Vector3.Zero;
     public Vector3 SpringRotation { get; set; } = Vector3.Zero;
+    public Vector3 AnchorA { get; set; } = Vector3.Zero;
+    public Vector3 AnchorB { get; set; } = Vector3.Zero;
 }
