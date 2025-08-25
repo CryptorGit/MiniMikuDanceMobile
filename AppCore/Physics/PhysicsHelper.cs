a72b030e96c6b2e9d99567ed53d6f629a39b1aaf
CryptorGit <167396277+CryptorGit@users.noreply.github.com>
2025-08-26 00:04:06 +0900

    Compute capsule volume and inertia

--- AppCore/Physics/PhysicsHelper.cs
+++ AppCore/Physics/PhysicsHelper.cs
@@ -57,16 +57,35 @@ public static class PhysicsHelper
     {
         var r = size.X * 0.5f;
         var h = size.Y;
+        // V = πr^2h + (4/3)πr^3
         var cyl = MathF.PI * r * r * h;
-        return cyl * density;
+        var sph = 4f / 3f * MathF.PI * r * r * r;
+        return (cyl + sph) * density;
     }
 
     private static Vector3 ComputeCapsuleInertia(Vector3 size, float mass)
     {
         var r = size.X * 0.5f;
         var h = size.Y;
-        var ixz = mass / 12f * (3f * r * r + h * h);
-        var iy = 0.5f * mass * r * r;
+        // 質量は円柱と半球2個の体積 V_cyl=πr^2h, V_sph=4/3πr^3 に基づき分配し、
+        // 半球の慣性モーメントは I_parallel=2/5 m r^2, I_perp=83/320 m r^2 を使用
+        var cylV = MathF.PI * r * r * h;
+        var sphV = 4f / 3f * MathF.PI * r * r * r;
+        var totalV = cylV + sphV;
+        var massCyl = mass * (cylV / totalV);
+        var massHemi = (mass - massCyl) * 0.5f;
+
+        var cylIxz = massCyl / 12f * (3f * r * r + h * h);
+        var cylIy = 0.5f * massCyl * r * r;
+
+        const float hemiIxzCoeff = 83f / 320f;
+        const float hemiIyCoeff = 2f / 5f;
+        var offset = h * 0.5f + 3f * r / 8f;
+        var hemiIxz = hemiIxzCoeff * massHemi * r * r + massHemi * offset * offset;
+        var hemiIy = hemiIyCoeff * massHemi * r * r;
+
+        var ixz = cylIxz + 2f * hemiIxz;
+        var iy = cylIy + 2f * hemiIy;
         return new Vector3(ixz, iy, ixz);
     }
 }
